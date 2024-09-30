700f63dee8089c3c8f153eed707decfade750f26
syedubaid <syedabuubaid@gmail.com>
2024-10-01 01:44:00 +0530

    commit

--- components/ui/dropdown.tsx
+++ components/ui/dropdown.tsx
@@ -1,6 +1,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Bars3Icon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 
 export default function Dropdown() {
@@ -17,22 +18,26 @@ export default function Dropdown() {
         className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
       >
         <div className="py-1">
+
           <MenuItem>
-            <a
-              href="#"
-              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-            >
+          <Link href="/about">
+            <div className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900">
               About
-            </a>
+            </div>
+          </Link>
           </MenuItem>
+
           <MenuItem>
             <a
-              href="#"
+              href="https://github.com/syedubaid11/tracker-jet"
+              target="_blank" // Opens the link in a new tab
+              rel="noopener noreferrer" // Security best practice
               className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
             >
-              Support
+              Contribute
             </a>
-          </MenuItem>
+         </MenuItem>
+          
           <MenuItem>
             <a
               href="#"
